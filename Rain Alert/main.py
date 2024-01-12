5a3454ff8523f1e814b231fd5c97463971b78df3
Fernando Destefani <henriqueorei@gmail.com>
2024-01-12 09:13:10 -0300

    updating rain alert

--- Rain Alert/main.py
+++ Rain Alert/main.py
@@ -7,12 +7,12 @@ print(env_variables)
 for key, value in env_variables.items():
     print(f'{key}: {value}')
 
-OWM_ENDPOINT = 'https://api.openweathermap.org/data/2.8/onecall'
-API_KEY = '61f57c1a6842056345e57cdfa44861b8'
+OWM_ENDPOINT = 'link_website'
+API_KEY = 'personal_password'
 LAT = -23.4253
 LONG = -51.9386
-account_sid = 'AC634e41741f89a0c6f08d997e22f58fe1'
-auth_token = '35a554423f79cbd05efc4e8a48d5c2bb'
+account_sid = 'private_account'
+auth_token = 'private_token'
 
 weather_params = {
     "lat": LAT,
@@ -40,7 +40,7 @@ if will_rain:
     message = client.messages \
         .create(
         body="Its gonna rain today. Remember to bring an ☂️",
-        from_='+12564884881',
-        to='+5584987590482'
+        from_='private_number',
+        to='+private_number'
     )
     print(message.status)
